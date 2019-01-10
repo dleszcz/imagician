a97e7c1c530dc5b0e86d92c34650b162bfd3a33f
Daniel Leszczyński <danielxleszczynski@gmail.com>
2019-01-10 12:41:48 +0100

    Clear expo project files

--- App.js
+++
@@ -1,4 +0,0 @@
-import { createAppContainer } from 'react-navigation';
-import AppNavigator from './navigation/AppNavigator';
-
-export default createAppContainer(AppNavigator);
--- navigation/AppNavigator.js
+++
@@ -1,8 +0,0 @@
-import { createStackNavigator } from 'react-navigation';
-import { HomeScreen } from './../screens/HomeScreen';
-import { ImageScreen } from './../screens/ImageScreen';
-
-export default createStackNavigator({
-  Home: HomeScreen,
-  Image: ImageScreen
-});
